0f3d2bd8e692a8fa4c8a680ce1609a946bcab582
VMNthemaster <varad.nadkarni09@gmail.com>
2022-09-08 09:19:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import WithNav from './components/Layout/WithNav'
+import WithoutNav from './components/Layout/WithoutNav'
+import Signin from './components/Signin'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <main>
+        <Router>
+          <Routes>
+            <Route element={<WithoutNav />}>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/signin" element={<Signin />} />
+            </Route>
+            <Route element={<WithNav />}>
+              {/* <Route path='/' element={<LandingPage />}  /> */}
+            </Route>
+          </Routes>
+        </Router>
+      </main>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="bg-gray-200">
+        <div className="flex flex-col h-[100vh] justify-center mx-[2vw] md:mx-[20vw] border-[0.5px] md:border-[2px] border-red-600">
+          <h1>Login to Quora For College</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage
